eef25569bc850a4e28e33bbd9e0c1b044262b282
Luke Mico <lukemico@me.com>
2018-04-03 07:46:03 +1000

    for loop added

--- framework/src/app.js
+++ framework/src/app.js
@@ -1,41 +1,23 @@
 var Endpoint1 = { lower: 0, upper: 100 }
+var Endpoint2 = { outputDetails: [{divisor: 3, output: "Boss"}, {divisor: 5, output: "Hogg" }]}
 
-var Endpoint2 = { outputDetails: 
-	[{divisor: 3, output: "Boss"},{ divisor: 5, output: "Hogg" }] }
+var rangeInfo = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100];
 
-
-Output1 
-	0 - 100
-
-Output2 
-	Every number that is divisible by 3 or 5 
-		
-var Endpoint1 = { lower: 0, upper: 100 }
-var Endpoint1 = { [0...100;] }	
-
-
-
-
-var Endpoint1 = [0...100];
-var Endpoint2 = '';
-
-if ( number % 3 === 0 || number % 5 === 0 ) {
-	returnStr += "Pling";
+for (rangeInfo = 0; rangeInfo < 100; rangeInfo++) {
+  console.log(rangeInfo);
 }
 
-
-
-if ( returnStr === '' ) {
-  returnStr += number;
+function testNum(rangeInfo) {
+	if (rangeInfo % 3 === 0) {
+    	return "Boss";
+} 	else if (rangeInfo % 5 === 0) {
+    	return "Hogg";
+} 	else {
+    	return "";
+  	}
 }
 
-
-
-
-
-
-
-
+console.log(testNum(rangeInfo));
 
 
 
--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var server = http.createServer(function(req, res) {
     var params = querystring.parse(url.parse(req.url).query);
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.write('Node Test - End Points');
+    res.write('Endpoint1');
+    res.write('Endpoint2');
+    res.write('rangeInfo');
+    res.write('divisiorInfo');
+
     res.end();
 });
 server.listen(9999);
