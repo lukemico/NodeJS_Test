ed807989c817c26b04ac78ef76fa3f84b2d57632
Luke Mico <lukemico@me.com>
2018-04-03 00:13:38 +1000

    2nd commit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
 
 var server = http.createServer(function(req, res) {
-res.writeHead(200);
-res.end('Hi everybody!');
+    var params = querystring.parse(url.parse(req.url).query);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write('Node Test - End Points');
+    res.end();
 });
-server.listen(8080);
-
-
-
-
+server.listen(9999);
